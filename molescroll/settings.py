3048877c037f1d9f150800341a83430dbf4fe9a4
Joseph Joseph <josephnjoseph43@gmail.com>
2023-12-26 11:25:07 +0530

    Update settings.py

--- molescroll/settings.py
+++ molescroll/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%&qu(2^(pccwmqyyz%uoo!rv_oygm^66^z5q(157ndo6vx5k!#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
